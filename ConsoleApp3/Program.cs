0f12fa9e30f3f4228974374f16dfa66ad88aac40
unknown <neyscampos@gmail.com>
2019-12-30 19:17:29 -0300

    initialize

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(WriteX);
+            t.Start();
+
+            Task task = Task.Run(() => {
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine("BackGround thread!!");
+                    Thread.Sleep(500);
+                }
+            });
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    Console.Write("O");
+            //}   
+        }
+
+        static void WriteX()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine("ForeGround thread!!");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
